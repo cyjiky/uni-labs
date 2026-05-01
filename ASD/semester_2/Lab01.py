7e12f9bd2889754b0eff8b29fd2e5150167ff838
Viky <vikyy905@gmail.com>
2026-05-01 13:02:39 +0200

    Add Lab01 to ASD (semester_2)

--- ASD/semester_2/Lab01.py
+++ ASD/semester_2/Lab01.py
@@ -1,50 +1,73 @@
-import math 
+import math
 
-# TODO 
-def recursion_1(x, n, i=1, F=1.0, cur_sum= 0.0):
+
+def recursion_1(x, n, i=1, F=None, cur_sum=0.0):
+    if F is None:
+        F = x
+
     if i > n:
-        return cur_sum 
-    
+        return cur_sum
+
     new_sum = cur_sum + F
-    calculate = (x**2) / (4*(i**2) - 2*i)
+    calculate = (x**2) / (4 * (i**2) + 2 * i)
     val = F * calculate
 
-    return recursion_1(x, n, i +1, val, new_sum)
+    return recursion_1(x, n, i + 1, val, new_sum)
+
 
 def recursion_2(x, n):
     if n == 1:
-        return 1.0, 1.0
-    
+        return x, x
+
     F, sum = recursion_2(x, n - 1)
-    i = n - 1 
-    calculate = (x**2) / (4*(i**2) - 2*i)
+    i = n - 1
+    calculate = (x**2) / (4 * (i**2) + 2 * i)
     cur_F = F * calculate
     cur_sum = sum + cur_F
 
     return cur_F, cur_sum
 
-def recursion_3(x, n, i=1, F=1.0):
-    if i > n: 
+
+def recursion_3(x, n, i=1, F=None):
+    if F is None:
+        F = x
+
+    if i > n:
         return 0.0
-    
-    calculate = (x**2) / (4*(i**2) - 2*i)
+
+    calculate = (x**2) / (4 * (i**2) + 2 * i)
     cur_F = F * calculate
 
-    return F + recursion_3(x, n, i+1, cur_F)
+    return F + recursion_3(x, n, i + 1, cur_F)
 
-# n = int(input("n: "))
-# x = float(input("x: "))
 
-n = 5
-x = 0.1
+def test_func(x, n):
+    cur_sum = 0.0
+    F = x
+    for i in range(1, n + 1):
+        cur_sum += F
+        calculate = (x**2) / (4 * (i**2) + 2 * i)
+        F = F * calculate
 
-res_1 = recursion_1(x, n)
-cur_F, res_2 = recursion_2(x, n)
-res_3 = recursion_3(x, n)
-result = math.cosh(x)
+    return cur_sum
 
-print(f"n: {n}, x: {x}")
-print(f"res1: {res_1}")
-print(f"res2: {res_2}")
-print(f"res3: {res_3}")
-print(f"result (chx): {result}")
+
+_n = 5
+x_val = [1.0, 2.0, 3.0, 4.0, 5.0]
+for i in x_val:
+    _sum = test_func(i, _n)
+    _val = math.sinh(i)
+
+    diff = abs(_sum - _val)
+    print(f"{i}\t{diff}")
+
+# res_1 = recursion_1(x, n)
+# cur_F, res_2 = recursion_2(x, n)
+# res_3 = recursion_3(x, n)
+# result = math.sinh(x)
+
+# print(f"n: {n}, x: {x}")
+# print(f"res1: {res_1}")
+# print(f"res2: {res_2}")
+# print(f"res3: {res_3}")
+# print(f"result (shx): {result}")
